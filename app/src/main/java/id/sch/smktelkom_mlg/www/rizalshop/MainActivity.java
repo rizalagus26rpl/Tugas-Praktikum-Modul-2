e1054fdee0f8e14a0c591afc75c1466346c2694b
Rizal Agus Atmaji <rizal_agus_26rpl@student.smktelkom-mlg.sch.id>
2019-02-27 19:41:41 +0700

    selesai

---
+++ app/src/main/java/id/sch/smktelkom_mlg/www/rizalshop/MainActivity.java
@@ -0,0 +1,99 @@
+package id.sch.smktelkom_mlg.www.rizalshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtnamapel, edtnamabar, edtjumlahbel, edtharga, edtuangbay;
+    private Button btnproses;
+    private Button btnhapus;
+    private Button btnexit;
+    private TextView txtnamapel;
+    private TextView txtnamabar;
+    private TextView txtjumlahbel;
+    private TextView txtharga;
+    private TextView txtuangbay;
+    private TextView txttotalbelanja;
+    private TextView txtuangkembali;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Penjualan");
+
+        edtnamapel = (EditText) findViewById(R.id.namapelanggan);
+        edtnamabar = (EditText) findViewById(R.id.namabarang);
+        edtjumlahbel = (EditText) findViewById(R.id.jumlahbeli);
+        edtharga = (EditText) findViewById(R.id.harga);
+        edtuangbay = (EditText) findViewById(R.id.uangbayar);
+        btnproses = (Button) findViewById(R.id.tombol1);
+        btnhapus = (Button) findViewById(R.id.tombol2);
+        btnexit = (Button) findViewById(R.id.tombol3);
+        txtnamapel = (TextView) findViewById(R.id.namapelanggan);
+        txtnamabar = (TextView) findViewById(R.id.namabarang);
+        txtjumlahbel = (TextView) findViewById(R.id.jumlahbeli);
+        txtharga = (TextView) findViewById(R.id.harga);
+        txtuangbay = (TextView) findViewById(R.id.uangbayar);
+        txttotalbelanja = (TextView) findViewById(R.id.totalbelanja);
+        txtuangkembali = (TextView) findViewById(R.id.uangkembali);
+
+        //memberikan action pada tombol proses
+
+        btnproses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String namapelanggan = edtnamapel.getText().toString().trim();
+                String namabarang = edtnamabar.getText().toString().trim();
+                String jumlahbeli = edtjumlahbel.getText().toString().trim();
+                String harga = edtharga.getText().toString().trim();
+                String uangbayar = edtuangbay.getText().toString().trim();
+
+                double jb = Double.parseDouble(jumlahbeli);
+                double h = Double.parseDouble(harga);
+                double ub = Double.parseDouble(uangbayar);
+                double total = (jb * h);
+                txttotalbelanja.setText("Total Belanja : " + total);
+
+                double uangkembalian = (ub - total);
+
+                if (ub < total){
+                    txtuangkembali.setText("Uang Kembali : Rp 0" );
+                }else{
+                    txtuangkembali.setText("Uang Kembali : " + uangkembalian);
+                }
+
+
+                //memberikan action pada tombol reset data
+            }
+        });
+        btnhapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtnamapel.setText(" ");
+                txtnamabar.setText(" ");
+                txttotalbelanja.setText(" Total Belanja : Rp 0");
+                txtharga.setText(" ");
+                txtuangbay.setText(" ");
+                txtuangkembali.setText("Uang Kembali : Rp 0");
+                txtjumlahbel.setText(" ");
+
+                Toast.makeText(getApplicationContext(),"Data sudah direset", Toast.LENGTH_LONG).show();
+
+                // memberikan action pada tombol keluar
+            }
+        });
+        btnexit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                moveTaskToBack(true);
+            }
+        });
+    }
+}
